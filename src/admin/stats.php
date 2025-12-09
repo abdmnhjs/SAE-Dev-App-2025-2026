72708f2260630e6bfaa900fa7e8ae8e7c3cef0ce
abdmnhjs <chaaraouiabdelaziz@gmail.com>
2025-12-09 16:21:30 +0100

    Calcul de la médiane qui s'affiche

--- src/admin/stats.php
+++ src/admin/stats.php
@@ -1,8 +1,8 @@
 <?php
-
 session_start();
+
 require '../includes/init.php';
-ensureUserAuthorized("adminweb");
+ensureUserAuthorized("tech");
 
 // --- Récupérer tous les systèmes d'exploitation ---
 $allOsQuery = "SELECT id, name FROM `os_list` ORDER BY name";
@@ -18,28 +18,29 @@ $allManufacturerResult = mysqli_query($loginToDb, $allManufacturerQuery);
 <head>
     <meta charset='UTF-8'>
     <title>Tech Panel</title>
-    <link rel="stylesheet" href="../css/adminweb/adminweb.css">
+    <link rel="stylesheet" href="../css/tech/tech-panel.css">
 </head>
 <body>
 <div class='sidebar'>
     <div class='sidebar-sections'>
-        <a class='sidebar-section' href='../logout.php' class='sections'>Se déconnecter</a>
-        <a class='sidebar-section' href='create-tech-form.php'>Créer un technicien</a>
-        <a class='sidebar-section' href='add-os-form.php'>Ajouter un système d'exploitation</a>
-        <a class='sidebar-section' href='add-manufacturer-form.php'>Ajouter un fabriquant</a>
-        <a class="sidebar-section" href="stats.php">Statistiques</a>
-        <a class="sidebar-section" href="probas.php">Probabilités</a>
-        <a class="sidebar-section" href="admin_panel-logs.php">Logs</a>
+            <a class='sidebar-section' href='../logout.php' class='sections'>Se déconnecter</a>
+
+        <a class='sidebar-section' href='tech-panel.php?section=screens'>Moniteurs</a>
+        <a class='sidebar-section' href='tech-panel.php?section=control-units'>Unités de contrôle</a>
+            <a class='sidebar-section' href='add-screen-form.php'>Ajouter un écran</a>
+    <a class='sidebar-section' href='add-control-unit-form.php'>Ajouter une unité de controle</a>
+        <a class="sidebar-section" href="stats.php">Effectuer des calculs de statistiques</a>
+        <a class="sidebar-section" href="probas.php">Effectuer des calculs de probabilités</a>
 
     </div>
 </div>
 
 <div>
-    <form method="post" action="actions/mean.php">
+    <form method="post" action="actions/stats/mean.php">
         <label for="os">Moyenne des ordinateurs possédant ce système d'exploitation : </label>
-        <select name="os_id" id="os" required>
+        <select name="os" id="os" required>
             <?php
-            // Afficher tous les manufactureurs
+            // Afficher tous les OS
             if ($allOsResult) {
                 while($row = mysqli_fetch_assoc($allOsResult)) {
                     echo "<option value='" . htmlspecialchars($row['id']) . "' >"
@@ -48,13 +49,12 @@ $allManufacturerResult = mysqli_query($loginToDb, $allManufacturerQuery);
             }
             ?>
         </select>
-        <input type="hidden" name="type" value="os">
-        <button type="submit" name="os">Calculer la moyenne</button>
+        <button type="submit">Calculer la moyenne</button>
     </form>
 
     <form method="post" action="actions/stats/mean.php">
-        <label for="manufacturer">Moyenne des moniteurs possédant ce fabricant : </label>
-        <select name="manufacturer_id" id="manufacturer" required>
+        <label for="os">Moyenne des moniteurs possédant ce fabricant : </label>
+        <select name="os" id="os" required>
             <?php
             // Afficher tous les OS
             if ($allManufacturerResult) {
@@ -93,7 +93,7 @@ $allManufacturerResult = mysqli_query($loginToDb, $allManufacturerQuery);
     ?>
     <?php
 
-    print_r($_SESSION['mean_result'] ?? '');
+    print_r($_SESSION['mean_result']);
 
     ?>
 
@@ -101,6 +101,14 @@ $allManufacturerResult = mysqli_query($loginToDb, $allManufacturerQuery);
     if(isset($_GET["standard-deviation"])){
         $standardDeviationResult = $_GET["standard-deviation"];
         echo "<p>L'écart-type de la ram des unités de contrôle vaut <span style='font-weight: bold'>".$standardDeviationResult."</span></p>";
+
+    }
+    ?>
+
+    <?php
+    if(isset($_GET["medial"])){
+        $medialResult = $_GET["medial"];
+        echo "<p>La médiane du temps de connexion sur la plateforme (en minutes) vaut <span style='font-weight: bold'>".$medialResult."min</span></p>";
 
     }
     ?>
