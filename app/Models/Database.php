42c2d3b2eee797f1d1cf3aae30e5e9ac6cff15f0
donqui6 <nathanael.gaudree.daem@gmail.com>
2025-12-10 18:07:10 +0100

    changed db password

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -9,11 +9,11 @@ class Database
         $host = $_ENV['DB_HOST'];
         $dbname = $_ENV['DB_NAME'];
         $user = $_ENV['DB_USER'];
-        $password = null; // rajouter l'env pour le rpi12
+        $password = $_ENV['DB_PASS'];
         $data_source_name = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
         try {
-            $this->connection = new PDO($data_source_name, $user, "", [
+            $this->connection = new PDO($data_source_name, $user, $password, [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //force pdo à écrire l'erreur si il y en a une
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //associative array mode
                 PDO::ATTR_EMULATE_PREPARES   => false, //force des requêtes sql préparer
