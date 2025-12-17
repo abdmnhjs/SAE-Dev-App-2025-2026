5ef90c38d9fb41f6fc0fb8577bc8df8fb96d2a19
abdmnhjs <chaaraouiabdelaziz@gmail.com>
2025-12-17 12:39:08 +0100

    Ajout de la description dans les logs

--- src/sysadmin/logs.php
+++ src/sysadmin/logs.php
@@ -27,6 +27,7 @@ $logs = mysqli_query($loginToDb, $queryLogs);
         <tr>
             <th>Nom d'utilisateur</th>
             <th>Adresse IP</th>
+            <th>Description</th>
             <th>Durée d'activité sur la plateforme</th>
             <th>Date de création du log</th>
         </tr>
@@ -37,6 +38,7 @@ $logs = mysqli_query($loginToDb, $queryLogs);
         echo "<tr>";
         echo "<td>" . $row['username'] . "</td>";
         echo "<td>" . $row['ip_address'] . "</td>";
+        echo "<td>" . $row['description'] . "</td>";
         echo "<td>" . $row['duration_seconds'] . "</td>";
         echo "<td>" . $row['log_time'] . "</td>";
         echo "</tr>";
