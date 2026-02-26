3dc37204ce3dc62964b400e7188b88fbdac7bee7
Abdelaziz Chaaraoui <22403728@me.uvsq.fr>
2026-02-26 14:01:04 +0100

    fix: message d'erreur de connexion plus près du formulaire

--- src/connexion.php
+++ src/connexion.php
@@ -13,10 +13,23 @@ session_start();
 <?php require __DIR__ . '/includes/sidebar.php'; ?>
 <main class="main-with-sidebar">
     <div class="connexion">
-        <?php include_once "html/connexion.html"; ?>
-        <?php if (isset($_GET['error'])) : ?>
-            <p style="color: red">Erreur de connexion</p>
-        <?php endif; ?>
+        <form class="block" method="post" action="action-login.php">
+            <img src="images/logo.png" alt="logo-img" width="80" height="80" class="center">
+            <h2 class="titre">Bon retour parmi nous</h2>
+            <p class="description">Connectez vous à votre compte infra' pour continuer</p>
+            <div class="connexion">
+                <label class="champs">Nom d'utilisateur</label>
+                <input type="text" name="username" class="input">
+                <label class="champs">Mot de passe</label>
+                <input type="password" name="password" class="input">
+                <div class="hero-buttons">
+                    <button type="submit" class="classic-button" style="color: #121212;">Se connecter</button>
+                </div>
+                <?php if (isset($_GET['error'])) : ?>
+                    <p style="color: red; margin-top: 8px;">Erreur de connexion</p>
+                <?php endif; ?>
+            </div>
+        </form>
     </div>
 </main>
 </body>
